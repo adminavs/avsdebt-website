1fa9127272a4f3f791f86d577e51aa923705b49d
adminavs <admin@avsdebt.com>
2026-06-30 22:44:43 +0800

    Add scroll event listener to change nav box shadow

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
 
+    if(window.scrollY > 50){
+        nav.style.boxShadow = "0 10px 25px rgba(0,0,0,.3)";
+    }else{
+        nav.style.boxShadow = "none";
+    }
+});
